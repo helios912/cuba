7e2cb9827ea00cb92fc662b834a5a8ad4ed60124
Mikola <das19901990a@gmail.com>
2024-04-04 21:43:55 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+  $('.slider__wrapper').slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./images/Arrow-left.svg" alt="" /></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="./images/Arrow-right.svg" alt="" /></button>',
+  });
 });
